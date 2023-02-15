ebc290499c7c109c4cb4a916885fad1705bb3283
Pedro Fachetti Carvalhom <pfachetti.pc@gmail.com>
2023-02-15 12:37:43 -0500

    delete later

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,24 @@
-import fastify, { FastifyServerOptions } from "fastify";
+import fastify, { FastifyRequest, FastifyServerOptions } from "fastify";
+import { FastifyRequestType } from "fastify/types/type-provider.js";
 
 const buildApp = async (opts: FastifyServerOptions) => {
     const app = fastify({
         logger: opts.logger,
     });
 
-    app.route({
+    interface IBody {
+        hello: string;
+    }
+
+    app.route<{ Body: IBody }>({
         method: "GET",
         url: "/",
         handler: async (request, reply) => {
-            reply.send({ hello: "world" });
+            console.log(request.body);
+
+            // const { hello } = request.body;
+
+            reply.send({ hello: "wolrd" });
         },
     });
 
