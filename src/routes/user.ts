d0cae5809a9bdcbdbf91144b4e243607732e40d6
Pedro Fachetti Carvalhom <pfachetti.pc@gmail.com>
2023-02-27 19:19:05 -0500

    routing issue

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,27 +1,14 @@
-import { FastifyPluginAsync } from "fastify"
-import userHandlers from "../handlers/user.js"
-import { UserType, User } from "../schemas/user.js"
+import { FastifyPluginAsync } from "fastify";
+import userHandlers from "../handlers/user.js";
+import { UserType, User } from "../schemas/user.js";
 
-
-
-const UserRoutes: FastifyPluginAsync = async (fastify, opts) => {
-    fastify.route<{ Body: UserType, Reply: UserType }>({
+export const UserRoutes: FastifyPluginAsync = async (fastify, opts) => {
+    fastify.route<{ Body: UserType; Reply: UserType }>({
         method: "POST",
-        url: "/",
+        url: "/user",
         schema: {
-            body: User
-
+            body: User,
         },
-        handler: userHandlers.loginHandler
+        handler: userHandlers.loginHandler,
     });
-
-
-
-
-
-
-
-
-
-
-}
+};
