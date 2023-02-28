a1476e8c532e1f83b78558d3150ed7fd59fb9cce
Rex Bianchi <rexbianchi@protonmail.com>
2023-02-27 19:17:09 -0500

    things

--- src/handlers/user.ts
+++ src/handlers/user.ts
@@ -1,7 +1,8 @@
 import { FastifyRequest, FastifyReply } from "fastify"
+import { UserType } from "../schemas/user.js";
 
 const userHandlers = {
-    loginHandler: async (request: FastifyRequestType, reply: FastifyReplyType) => {
+    loginHandler: async (request: FastifyRequest<{ Body: UserType, Reply: UserType }>, reply: FastifyReply) => {
         const { username, password } = request.body;
         return reply.send(request);
 
